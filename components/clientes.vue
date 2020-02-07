7e2ee72bc48b6cb008ec992f2abe9df2dfa8d2e7
iamcgd <hola@iamcgd.com>
2020-02-07 14:46:41 -0300

    change clientes sizes

--- components/clientes.vue
+++ components/clientes.vue
@@ -5,8 +5,8 @@
       <p class="f5 ttu green-gl normal tracked">experiencia</p>
       <h2 class="f2-l f4 blue-gl mt0">Marcas lideres en el mercado</h2>
     </div>
-    <ul class="flex flex-wrap justify-center pa0 list clients">
-      <li v-for="c in clients" :key="c.name" class="ph2 pv3" data-aos="fade-up">
+    <ul class="flex flex-wrap justify-center items-center pa0 list clients">
+      <li v-for="c in clients" :key="c.name" class="ph4 pv3" data-aos="fade-up">
         <a :href="c.link" target="_blank" rel="noopener">
           <img :src="`/clients/${c.name}.png`" class="dib" :alt="c.name">
         </a>
